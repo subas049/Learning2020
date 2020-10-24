bdf4e207c9ec75182f9830d37346ec968e105859
Subash Perumal <subas049@gmail.com>
2020-10-24 10:41:42 +0530

    changes in file

--- Java/helloworld.java
+++ Java/helloworld.java
@@ -5,5 +5,5 @@ public static void main(String[] args)
 System.out.println("hello subash, you are welcome to java world");
 System.out.println("thanks for your guruji sravan kumar");
 return;
-System.out.println("check for unreachable statement error");
+//System.out.println("check for unreachable statement error");
 }
---
+++ Java/secondworld.java
@@ -0,0 +1,8 @@
+class secondworld
+{
+public static void main(String[] args)
+{
+System.out.println("hello subash, you are welcome to java world");
+System.out.println("this is a second pgm");
+
+}
