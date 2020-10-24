f63fe10cbd5d56cb27db92d5d3d6c51196893c0f
Subash Perumal <subas049@gmail.com>
2020-10-24 10:17:12 +0530

    basic helloworld java file

---
+++ Java/helloworld.java
@@ -0,0 +1,9 @@
+class helloworld
+{
+public static void main(String[] args)
+{
+System.out.println("hello subash, you are welcome to java world");
+System.out.println("thanks for your guruji sravan kumar");
+return;
+System.out.println("check for unreachable statement error");
+}
