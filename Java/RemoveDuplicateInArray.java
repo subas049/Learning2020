8e055e95495d9e15c6702d184f44a67e6487a67e
Subash Perumal <subas049@gmail.com>
2020-10-31 10:23:20 +0530

    modified array sorting pgm

--- Java/RemoveDuplicateInArray.java
+++ Java/RemoveDuplicateInArray.java
@@ -9,13 +9,18 @@ public static int removeDuplicateElements(int arr[], int n){
         int j = 0;  
         for (int i=0; i<n-1; i++){  
             if (arr[i] != arr[i+1]){  
-                temp[j] = arr[i];  
-				j++;
+                temp[j] = arr[i];
+				j++;				
+				
             }  
-         }  
-        temp[j] = arr[n-1];
+         }
+		 System.out.println( "j value is : " +j);  
+		 System.out.println( "last value of array is : " +arr[n-1]);  
+		temp[j] = arr[n-1];
+		j++;	
+		System.out.println( "j value is : " +j);
+
 		
-        // Changing original array  
         for (int i=0; i<j; i++){  
             arr[i] = temp[i];  
         }  
